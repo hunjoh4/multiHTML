24257b04f29f77b4823b1bdc1df5fd2bf7d7b73d
hunjoh4 <hunjoh@koreanair.com>
2019-08-22 17:19:35 +0900

    HTML&CSS 4일차
    - 툴팁, 새창열기
    -정의형 목록
    - type="search"
    - 탭 콘텐츠
    - figure

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,8 +1,22 @@
 let items = $('.menu-item');
+let section = $('.board section');
+let tab = $('.tab');
+let tabsubject = $('.board ul a');
 
 items.attr('tabindex','0');
+tabsubject.addClass('icon-dot-circled');
+tab.attr('tabindex','0');
 
 items.on('mouseover focusin', function(){
   items.removeClass('menu-act');
   $(this).addClass('menu-act')
 });
+
+tab.on('click keyup', function(e){
+  if(e.type === 'click' || (e.type === 'keyup' && e.keydode === 13)){
+    section.removeClass('tab-act');
+    $(this).parent().addClass('tab-act');
+  }
+  
+});
+
